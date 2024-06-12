750384124b24be39a4df813aace6c8c8939256a7
mukhtar ali <mukhtar.ali17@gmail.com>
2024-06-13 03:09:48 +0500

    point 4 and 5

--- src/app/modules/admin/apps/reports/reports.component.ts
+++ src/app/modules/admin/apps/reports/reports.component.ts
@@ -1,14 +1,19 @@
 import { Component } from '@angular/core';
 import { MatIconModule } from '@angular/material/icon';
 import { MatButtonModule } from '@angular/material/button';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { CommonModule } from '@angular/common';
+import { MatDatepickerModule } from '@angular/material/datepicker';
+import { MatInputModule } from '@angular/material/input';
 
 @Component({
   selector: 'app-reports',
   standalone: true,
-  imports: [MatIconModule,MatButtonModule],
+  imports: [MatIconModule,MatButtonModule,MatFormFieldModule,CommonModule,MatDatepickerModule,MatInputModule],
   templateUrl: './reports.component.html',
   styleUrl: './reports.component.scss'
 })
 export class ReportsComponent {
+formFieldHelpers: any;
 
 }
